c5ac7c4905c05dd8efedc7477ca4c8c734926df1
yusuf0786 <101986674+paigamewaris@users.noreply.github.com>
2022-03-26 12:59:10 +0530

    another commit

--- pages/js/hazrat_haji_rafi_queali_shah_warsi.js
+++ pages/js/hazrat_haji_rafi_queali_shah_warsi.js
@@ -23,7 +23,6 @@ function close_resp_nav() {
 
 
 // waqiyat JS
-/*
 let read_more_btn = document.querySelector(".read_more")
 let read_less_btn = document.querySelector(".read_less")
 let waqiyat_div = document.querySelector(".waqiyat")
@@ -41,15 +40,38 @@ function close_waqiyat() {
     read_less_btn.style.display = "none"
     read_more_btn.style.display = "block"
 }
-*/
+
 // waqiyat modal JS
 let waqiyat = document.querySelectorAll("#waqiya")
 let waqiyat_pages = document.querySelectorAll(".waqiya_pg")
+let waqiya_pg_close = document.querySelectorAll(".waqiya_pg_close")
 
+// for modal open
 for(let i = 0; i <= waqiyat.length-1; i++){
     waqiyat[i].addEventListener('click', function(e){
         waqiyat_pages[i].style.display = "block"
+        waqiyat_pages[i].classList.add("slideIn_right_anim")
+        setTimeout(function() {
+            waqiyat_pages[i].classList.remove("slideIn_right_anim")
+        }, 500);
         e.preventDefault()
+    })
+}
+// for close waqiya pg
+for(let i = 0; i <= waqiya_pg_close.length-1; i++) {
+    waqiya_pg_close[i].addEventListener('click', function(){
+        waqiyat_pages[i].classList.add("slideOut_left_anim")
+        setTimeout(function() {
+            waqiyat_pages[i].classList.remove("slideOut_left_anim")
+            waqiyat_pages[i].style.display = "none"
+        }, 500);
+    })
+
+    waqiya_pg_close[i].addEventListener('mouseover', function(){
+        waqiya_pg_close[i].classList.add("rotate_anim")
+    })
+    waqiya_pg_close[i].addEventListener('mouseleave', function(){
+        waqiya_pg_close[i].classList.remove("rotate_anim")
     })
 }
 
