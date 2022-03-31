495fdfea0aa734acbc0918408bd0dfd351fbbff0
yusuf0786 <paigamewaris707@gmail.com>
2022-03-31 23:19:14 +0530

    another commit

---
+++ pages/js/renowed_warsi_faqeer.js
@@ -0,0 +1,77 @@
+/* JS for navigation starts here */
+// targeting the elements
+let nav_li = document.querySelectorAll(".mynavigation .mynav li")
+let menu_btn = document.querySelector(".menu_btn")
+let resp_nav = document.querySelector(".resp_nav")
+let resp_nav_close = document.querySelector(".close")
+let resp_nav_li = document.querySelectorAll(".resp_nav .nav_pg_1 ul li")
+let overlay = document.querySelector(".overlay")
+
+// doing the functions
+menu_btn.addEventListener("click", function(){
+    resp_nav.classList.add("My_slideInRight")
+
+    overlay.style.display = "block"
+})
+resp_nav_close.addEventListener("click", close_resp_nav)
+
+function close_resp_nav() {
+    resp_nav.classList.remove("My_slideInRight")
+    overlay.style.display = "none"
+}
+/* JS for navigation ends here */
+
+
+
+// personalities_section JS
+// let read_more_btn = document.querySelector(".read_more")
+// let read_less_btn = document.querySelector(".read_less")
+// let waqiyat_div = document.querySelector(".waqiyat")
+
+// read_more_btn.addEventListener('click', open_waqiyat)
+// read_less_btn.addEventListener('click', close_waqiyat)
+
+// function open_waqiyat() {
+//     waqiyat_div.style.height = "auto"
+//     read_more_btn.style.display = "none"
+//     read_less_btn.style.display = "block"
+// }
+// function close_waqiyat() {
+//     waqiyat_div.style.height = "130px"
+//     read_less_btn.style.display = "none"
+//     read_more_btn.style.display = "block"
+// }
+
+// personalities_section modal JS
+let personality = document.querySelectorAll("#personality")
+let personality_pg = document.querySelectorAll(".personality_pg")
+let pers_pg_close = document.querySelectorAll(".pers_pg_close")
+
+// for modal open
+for(let i = 0; i <= personality.length-1; i++){
+    personality[i].addEventListener('click', function(e){
+        personality_pg[i].style.display = "block"
+        personality_pg[i].classList.add("slideIn_right_anim")
+        setTimeout(function() {
+            personality_pg[i].classList.remove("slideIn_right_anim")
+        }, 500);
+        e.preventDefault()
+    })
+}
+// for close personality_pg
+for(let i = 0; i <= pers_pg_close.length-1; i++) {
+    pers_pg_close[i].addEventListener('click', function(){
+        personality_pg[i].classList.add("slideOut_left_anim")
+        setTimeout(function() {
+            personality_pg[i].classList.remove("slideOut_left_anim")
+            personality_pg[i].style.display = "none"
+        }, 500);
+    })
+
+    pers_pg_close[i].addEventListener('mouseover', function(){
+        pers_pg_close[i].classList.add("rotate_anim")
+    })
+    pers_pg_close[i].addEventListener('mouseleave', function(){
+        pers_pg_close[i].classList.remove("rotate_anim")
+    })
+}
