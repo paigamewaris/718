9febe14159863f66a3a75f37476ac08f5045db6b
paigamewaris <paigamewaris707@gmail.com>
2022-06-26 17:40:13 +0530

    another commit

--- js/main.js
+++ js/main.js
@@ -1,63 +1,85 @@
 let new_badge = document.querySelector("[data-new-badge]")
 new_badge.classList.add("new_anim")
 
+// 
+let NavSearchButton = document.querySelector("[data-nav-search-button]")
+let SearchSection = document.querySelector(".search-section")
+let CloseSearch = document.querySelector(".close-search")
+NavSearchButton.addEventListener("click", function(){
+    SearchSection.style.display = "block"
+})
+CloseSearch.addEventListener("click", function(){
+    SearchSection.style.display = "none"
+})
+resp_nav_open_search.addEventListener("click", function(e){
+    e.preventDefault()
+    SearchSection.style.display = "block"
+})
+
 // search bar
-let SearchBox = document.querySelector(".search-box")
-let SearchButton = document.querySelector("[data-search-button]")
-let InputSearch = document.getElementById("search")
-SearchButton.addEventListener("click", function(){
-    InputSearch.focus()
-})
-
-InputSearch.addEventListener("focus", function(){
-    SearchBox.style.outline = "3px solid #fff"
-    SearchBox.style.boxShadow = "0 0 8px 3px #fff"
-})
-InputSearch.addEventListener("blur", function(){
-    SearchBox.style.outline = 0
-    SearchBox.style.boxShadow = "none"
-})
-
-
-// search function
-let userCardTemplate = document.querySelector("[data-user-template]")
-let userCardContainer = document.querySelector("[data-user-cards-container]")
-
-let users = []
-
-InputSearch.addEventListener("input", function(e){
-    let value = e.target.value.toLowerCase()
-    !value ? userCardContainer.classList.add("hide") : userCardContainer.classList.remove("hide")
-
-    users.forEach(user => {
-        let isVisible = user.ContentName.toLowerCase().includes(value) || user.ContentCategory.toLowerCase().includes(value)
-        user.element.classList.toggle("show", isVisible)
+let SearchBox = document.querySelectorAll(".search-box")
+let SearchButton = document.querySelectorAll("[data-search-button]")
+let InputSearch = document.querySelectorAll("[data-search-input]")
+for(let i = 0; i<=SearchBox.length-1; i++){
+    SearchButton[i].addEventListener("click", function(){
+        InputSearch[i].focus()
     })
-
-})
-
-let userAPI = fetch("https://paigamewaris.github.io/718/js/ContentData.json")
-
-async function getuserdata() {
-    let userdata = await userAPI
-    let userdata_json = userdata.json()
-    let data = await userdata_json
-    return data
+    
+    InputSearch[i].addEventListener("focus", function(){
+        SearchBox[i].style.outline = "3px solid #fff"
+        SearchBox[i].style.boxShadow = "0 0 8px 3px #fff"
+    })
+    InputSearch[i].addEventListener("blur", function(){
+        SearchBox[i].style.outline = 0
+        SearchBox[i].style.boxShadow = "none"
+    })
+    
+    
+    // search function
+    let userCardTemplate = document.querySelectorAll("[data-user-template]")
+    let userCardContainer = document.querySelectorAll("[data-user-cards-container]")
+    
+    let users1
+    let users2
+    let usersArray = [users1, users2]
+    
+    InputSearch[i].addEventListener("input", function(e){
+        let value = e.target.value.toLowerCase()
+        !value ? userCardContainer[i].classList.add("hide") : userCardContainer[i].classList.remove("hide")
+    
+        usersArray[i].forEach(user => {
+            let isVisible = user.ContentName.toLowerCase().includes(value) || user.ContentCategory.toLowerCase().includes(value)
+            user.element.classList.toggle("show", isVisible)
+        })
+    })
+    
+    let userAPI = fetch("https://paigamewaris.github.io/718/js/ContentData.json")
+    
+    async function getuserdata() {
+        let userdata = await userAPI
+        let userdata_json = userdata.json()
+        let data = await userdata_json
+        return data
+    }
+    
+    let storevalue1
+    let storevalue2
+    let storevalueArray = [storevalue1, storevalue2]
+    storevalueArray[i] = getuserdata()
+    storevalueArray[i].then(function(res){
+            usersArray[i] = res.map(user => {
+                let card = userCardTemplate[0].content.cloneNode(true).children[0]
+                let header = card.querySelector("[data-header]")
+                let body = card.querySelector("[data-body]")
+                header.textContent = user.ContentName
+                header.setAttribute("href", user.ContentName)
+                body.textContent = user.ContentCategory
+                userCardContainer[i].append(card)
+                return {
+                    ContentName: user.ContentName,
+                    ContentCategory: user.ContentCategory,
+                    element: card
+                }
+        });
+    })
 }
-
-let storevalue = getuserdata()
-storevalue.then(function(res){
-        users = res.map(user => {
-            let card = userCardTemplate.content.cloneNode(true).children[0]
-            let header = card.querySelector("[data-header]")
-            let body = card.querySelector("[data-body]")
-            header.textContent = user.ContentName
-            body.textContent = user.ContentCategory
-            userCardContainer.append(card)
-            return {
-                name: user.name,
-                email: user.email,
-                element: card
-            }
-    });
-})
--- js/navigation.js
+++ js/navigation.js
@@ -5,6 +5,8 @@ let menu_btn = document.querySelector(".menu_btn")
 let resp_nav = document.querySelector(".resp_nav")
 let resp_nav_close = document.querySelector(".close")
 let resp_nav_li = document.querySelectorAll(".resp_nav .nav_pg_1 ul li")
+let resp_nav_a = document.querySelectorAll(".resp_nav .nav_pg_1 ul li a")
+let resp_nav_open_search = document.querySelector(".open-search")
 let overlay = document.querySelector(".overlay")
 
 // doing the functions
@@ -14,8 +16,15 @@ menu_btn.addEventListener("click", function(){
 
     overlay.style.display = "block"
 })
-resp_nav_close.addEventListener("click", function() {
+resp_nav_close.addEventListener("click", CloseNav)
+
+resp_nav_a.forEach(function(a) {
+    console.log(a)
+    a.addEventListener("click", CloseNav)
+})
+
+function CloseNav() {
     body.classList.remove("modal-open-body")
     resp_nav.classList.remove("My_slideInRight")
     overlay.style.display = "none"
-})
+}
