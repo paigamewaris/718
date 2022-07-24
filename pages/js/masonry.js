aabd7daa0393c3a655691dd3b3d165616da504e4
paigamewaris <paigamewaris707@gmail.com>
2022-07-24 22:18:06 +0530

    another commit

--- pages/js/masonry.js
+++ pages/js/masonry.js
@@ -1,8 +1,11 @@
 let sectionPhotos = document.querySelector("[data-photos]")
-let dataSectionPhotosContainer = document.querySelector("[data-section-photos-container]")
 let count = sectionPhotos.dataset.photos
+let dataSectionPhotosContainer = document.querySelector("[data-section-photos-container]")
+    
 
-for(let i = 1; i <= count; i++){
+for(let i = 0; i <= count - 1; i++){
     let codeSnipit = dataSectionPhotosContainer.content.cloneNode(true).children[0]
+    let newPath = `img/masonry/${i+1}.jpg`
+    codeSnipit.firstElementChild.src = newPath
     sectionPhotos.append(codeSnipit)
 }
